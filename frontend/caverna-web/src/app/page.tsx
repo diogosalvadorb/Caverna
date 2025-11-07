426f99cb73e47ab38155bc0a86dc14d1a8affcf9
diogosalvadorb <diogosalvadorb@gmail.com>
2025-11-06 22:37:07 -0300

    arquivo env na VM

--- frontend/caverna-web/src/app/page.tsx
+++ frontend/caverna-web/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-const API_URL = "http://localhost:5000/api/caverna";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/caverna";
 
 type CavernaState = {
   fogueiras: number;
